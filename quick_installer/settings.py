345908b82b5261253d2284efe5803c1075dd9fb7
David Fialho <fialho.david@protonmail.com>
2018-01-21 18:30:19 +0000

    Add all application to SYSTEM_APPS

--- quick_installer/settings.py
+++ quick_installer/settings.py
@@ -1,7 +1,16 @@
 """File containing all setting used by the tool"""
-from quick_installer.applications import SignalApplication
+from quick_installer.applications import *
 
 # Applications to be installed when performing a system install
 SYSTEM_APPS = [
-    SignalApplication
+    SignalApplication,
+    ChromeApplication,
+    SeafileApplication,
+    EnpassApplication,
+    AtomApplication,
+    SlackApplication,
+    IntellijIDEAApplication,
+    PyCharmApplication,
+    SublimeApplication,
+    SpotifyApplication
 ]
