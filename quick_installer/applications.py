47d8f0c287ede61bf8d10514a66a43bbcbb35338
David Fialho <fialho.david@protonmail.com>
2018-01-21 16:43:31 +0000

    Add first application: 'Signal'

---
+++ quick_installer/applications.py
@@ -0,0 +1,27 @@
+from quick_installer.application import Application
+from quick_installer.enable import Enabled
+from quick_installer.installers import apt
+
+
+@Enabled
+class SignalApplication(Application):
+
+    @property
+    def name(self) -> str:
+        return "signal"
+
+    def setup(self):
+        apt.add_source(
+            repository="deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main",
+            name="signal",
+            key_url="https://updates.signal.org/desktop/apt/keys.asc"
+        )
+
+    def install(self):
+        apt.install('signal-desktop')
+
+    def cleanup(self):
+        pass
+
+    def is_installed(self) -> bool:
+        return apt.is_package_installed('signal-desktop')
